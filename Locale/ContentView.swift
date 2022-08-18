363b98fd49e8da9f352e9bace9e34b99494ea459
vishal kalola <vkalola@een.com>
2022-08-18 16:56:35 +0200

    Initial Commit

---
+++ Locale/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Locale
+//
+//  Created by vishal on 8/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Locale/LocaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleApp.swift
+//  Locale
+//
+//  Created by vishal on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LocaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
